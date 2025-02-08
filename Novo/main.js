c740194dd9ba89b6cb8ddeb834962b01fe7cec0a
Magezi Joshua Wisdom <101383274+KingWise69@users.noreply.github.com>
2025-02-08 18:29:57 +0300

    Update main.js

--- Novo/main.js
+++ Novo/main.js
@@ -14,6 +14,28 @@ navLinks.addEventListener("click", (e) => {
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
+const faq = document.querySelector(".faq__grid");
+
+faq.addEventListener("click", (e) => {
+  const target = e.target;
+  const faqCard = target.closest(".faq__card");
+  if (target.classList.contains("ri-arrow-down-s-line")) {
+    if (faqCard.classList.contains("active")) {
+      faqCard.classList.remove("active");
+    } else {
+      Array.from(faq.children).forEach((item) => {
+        item.classList.remove("active");
+      });
+      faqCard.classList.add("active");
+    }
+  }
+});
+
+ScrollReveal().reveal(".faq__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+
 const scrollRevealOption = {
   distance: "50px",
   origin: "bottom",
